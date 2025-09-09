b8ee9f1c210ae4903a163e6aafa658f568dd00e3
Caleb Callaway <caleb.callaway@intel.com>
2025-09-09 19:12:02 +0000

    Work around dependency issues for Ubuntu 24.04
    `apt build-deps -y mesa` is installing a version of `libllvmspirvlib-dev`
    that Mesa rejects.
    This commit works around the issue by manually installing the correct version
    of the package when building in the noble chroot.

--- mesa-build.sh
+++ mesa-build.sh
@@ -178,6 +178,9 @@ install_spirv_deps() {
 
 	# # Handle SPIR-V to LLVM translator
 	case "$1" in
+		*"noble"*)
+		schroot -c $1 -- sh -c "sudo apt -y install libllvmspirvlib-19-dev"
+		;
 		*"plucky"*)
 		schroot -c $1 -- sh -c "sudo apt -y install libllvmspirvlib-20-dev"
 		;;
